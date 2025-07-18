112b379d331130b893eed0b18357f95563e4d430
VishalOz <vishal.20240036@iit.ac.lk>
2025-07-18 08:36:51 +0530

    position fixes and responsiveness

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -13,21 +13,23 @@ const Nav = () => {
     item.toLowerCase() === 'all' ? '#all' : `#${item.toLowerCase()}`;
 
   return (
-    <div className="w-full flex justify-center py-4">
-      <nav className="flex bg-gray-200 p-2 rounded-full gap-3 text-gray-700 text-md font-sm">
+    <div className="w-full flex justify-center py-15 px-2">
+      <nav
+        className="flex overflow-x-auto bg-gray-200 p-2 rounded-full gap-3 text-gray-950 font-bold text-md font-sm scrollbar-hide"
+      >
         {navBar.map((item) => (
           <a
             key={item}
             href={formatLink(item)}
             onClick={(e) => {
-              e.preventDefault(); // Stop default jump
+              e.preventDefault();
               setSelectedIcon(item);
               const target = document.querySelector(formatLink(item));
               if (target) {
-                target.scrollIntoView({ behavior: 'smooth' }); // Scroll to section
+                target.scrollIntoView({ behavior: 'smooth' });
               }
             }}
-            className={`cursor-pointer px-3 py-1 rounded-full transition ${
+            className={`cursor-pointer px-4 py-1 rounded-full whitespace-nowrap transition ${
               selectedIcon === item ? 'bg-white text-black shadow' : ''
             }`}
           >
