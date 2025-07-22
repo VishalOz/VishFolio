43980b2897a33d57618dc7a72d169bfe0a1dd448
VishalOz <vishal.20240036@iit.ac.lk>
2025-07-23 01:41:42 +0530

    map, bento fixes

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import Nav from './Nav'
 import { motion, AnimatePresence } from 'framer-motion'
 
-import dp1 from '../src/assets/dp2.png'
-import dp2 from '../src/assets/dp3.png'
+import dp2 from '../src/assets/dp2.png'
+import dp1 from '../src/assets/dp3.png'
 
 const Home = () => {
   const images = [dp1, dp2];
@@ -44,18 +44,18 @@ const Home = () => {
       <Nav />
       <div className="max-w-7xl mx-auto grid grid-cols-6 grid-rows-6 gap-4"> 
         {/* First box*/ }
-        <div className="relative col-span-2 row-span-2 bg-white text-white rounded-2xl p-6 flex flex-col justify-between items-start">
+        <div className="relative col-span-3 row-span-1 bg-white text-white rounded-4xl px-10 py-6 flex flex-col justify-between items-start shadow-lg">
 
           {/* Top-right button */}
           <button
             onClick={handleImageSwitch}
-            className="absolute top-4 right-4 text-black py-1 px-4 rounded-full border border-gray-300 hover:border-gray-800 cursor-pointer  transition"
+            className="absolute top-8 right-6 text-black py-1 px-4 rounded-full border border-gray-300 hover:border-gray-800 cursor-pointer  transition"
           >
-            Toggle
+            Toggle Developer
           </button>
 
           {/* Image */}
-          <div className="sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[200px]flex items-center justify-start overflow-hidden rounded-2xl mb-4 mt-8">
+          <div className="sm:w-[200px] sm:h-[200px] md:w-[300px] md:h-[200px]flex items-center justify-start overflow-hidden rounded-4xl mb-4 mt-8">
 
             <AnimatePresence mode="wait">
               <motion.img
@@ -73,9 +73,9 @@ const Home = () => {
 
           {/* Text */}
           <div className="flex justify-center items-center">
-            <p className="text-gray-600 text-md font-semibold">
+            <p className="text-gray-800 text-md font-semibold">
             I am <span className="font-bold text-xl text-black">Vishal</span>, a developer and designer from Sri Lanka.  
-            I'm passionate about <span className="font-semibold text-indigo-600">React, Java, .NET</span>, and I enjoy blending code with creativity through <span className="font-semibold text-indigo-600">cinematography</span> and <span className="font-semibold text-indigo-600">music</span>.  
+            I'm passionate about <span className="font-semibold text-indigo-600">React, Java, .NET</span>,  
             Always curious, I strive to build experiences that are both functional and visually compelling.
 
             </p>
@@ -83,10 +83,27 @@ const Home = () => {
         </div>
 
         {/* Second box*/ }
-        <div className="col-span-2 row-span-2 bg-green-200 rounded-2xl shadow p-6">
-          <h3 className="text-xl font-semibold text-gray-800">Blog Highlight</h3>
-          <p className="text-gray-600 mt-2">“My journey into UX design and development.”</p>
+        <div className="sm:w-full sm:h-[369px] md:w-full md:h-[369px] rounded-4xl overflow-hidden shadow-lg">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63322.89988123552!2d79.82967404999999!3d6.921922699999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae259292084b94b%3A0x810fc39fdce6e0b!2sColombo!5e0!3m2!1sen!2slk!4v1621942393063!5m2!1sen!2slk"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+            title="My Location"
+          ></iframe>
         </div>
+
+        {/* Third box*/ }
+        <div className="col-span-2 row-span-1 bg-white rounded-4xl p-6 shadow-lg">
+        </div>
+        {/* Forth box*/ }
+        <div className="col-span-1 row-span-1/2 bg-white rounded-4xl p-6 shadow-lg">
+        <img src="../src/assets/insta.gif" alt="Insta" className="w-full object-cover"/>
+        </div>
+
       </div>
     </div>
   )
