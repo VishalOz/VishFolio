9590e25aa4c23f70847826b348079229496dc3e3
VishalOz <vishal.20240036@iit.ac.lk>
2025-08-05 10:46:09 +0530

    Feat(Chaged the entire flexbox)

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,18 +6,22 @@ const Home = () => {
   return (
     <>
       <Nav />
-      <div className="grid grid-cols-6 grid-rows-7 gap-2 w-[520px] h-[608px] p-20">
-        <div className="md:col-span-2 sm:col-span-2 md:row-span-2 sm:row-span-2 bg-white shadow-lg rounded-4xl">
-          <img src="../src/assets/dp2.png" alt="" className="rounded-full object-cover" />
-          <div className="flex justify-center items-center">
-            <p className="text-gray-800 text-md font-semibold">
-            I am <span className="font-bold text-xl text-black">Vishal</span>, a developer and designer from Sri Lanka.  
-            I'm passionate about <span className="font-semibold text-indigo-600">React, Java, .NET</span>,  
+      <div className="grid grid-cols-6 grid-rows-7 gap-2 w-[1240px] h-[1496px] p-5">
+          <div className="row-span-2 col-span-3 bg-white rounded-4xl p-5">
+            <img src="../src/assets/ogdo.png" alt="DpImage" className="w-35 h-50" />
+            <p className="mt-4 text-md leading-relaxed text-gray-700">
+            I am <span className="font-bold text-indigo-600 text-xl">Vishal</span>, a developer and designer from Sri Lanka.  
+            I'm passionate about <span className="font-semibold text-indigo-600">React, Java, .NET</span>.  
             Always curious, I strive to build experiences that are both functional and visually compelling.
             </p>
           </div>
-        </div>
-        </div>
+          <div className="row-span-2 col-span-2 bg-white rounded-4xl p-5">
+
+          </div>
+          <div className="row-span-2 col-span-1 bg-white rounded-4xl p-5">
+
+          </div>
+      </div>
     </>
   )
 }
