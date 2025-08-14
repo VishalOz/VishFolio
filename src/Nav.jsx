e3c87c3941f0fbff7cc8e518ef0fd0c2acf136d6
VishalOz <vishal.20240036@iit.ac.lk>
2025-08-14 18:25:52 +0530

    feat(navigaton filtering)

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,37 +1,31 @@
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React from 'react';
+import { motion } from 'framer-motion';
 
-const Nav = () => {
-  const [selected, setSelected] = useState('All');
-
+const Nav = ({ selected, setSelected }) => {
   const navItems = ['All', 'About', 'Projects', 'Media'];
 
   return (
-    <>
-      <div className="flex justify-center w-full">
-        <nav className={`relative flex justify-center items-center gap-1 bg-gray-200 rounded-full p-4 w-80 text-sm`}>
-          {navItems.map((item) => (
-            <div key={item} className="relative hover:text-gray-400">
-              {selected === item && (
-                <motion.div
-                  layoutId="nav-highlight"
-                  className="absolute top-1/2 left-0 right-0 -translate-y-1/2 h-10 rounded-full bg-white shadow-md"
-                  transition={{ type: 'spring', stiffness: 500, damping: 60 }}
-                />
-              )}
-              <a
-                onClick={() => setSelected(item)}
-                className={`relative items-center cursor-pointer px-4 py-1 rounded-full transition duration-200 z-10 ${
-                  selected === item ? 'text-black' : ''
-                }`}
-              >
-                {item}
-              </a>
-            </div>
-          ))}
-        </nav>
-      </div>
-    </>
+    <div className="flex justify-center w-full">
+      <nav className="relative flex justify-center items-center gap-1 bg-gray-200 rounded-full p-4 w-80 text-sm">
+        {navItems.map((item) => (
+          <div key={item} className="relative hover:text-gray-400">
+            {selected === item && (
+              <motion.div
+                layoutId="nav-highlight"
+                className="absolute top-1/2 left-0 right-0 -translate-y-1/2 h-10 rounded-full bg-white shadow-md"
+                transition={{ type: 'spring', stiffness: 500, damping: 60 }}
+              />
+            )}
+            <a
+              onClick={() => setSelected(item)}
+              className={`relative items-center cursor-pointer px-4 py-1 rounded-full transition duration-200 z-10 ${selected === item ? 'text-black' : ''}`}
+            >
+              {item}
+            </a>
+          </div>
+        ))}
+      </nav>
+    </div>
   );
 };
 
